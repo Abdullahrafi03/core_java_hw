40f4dcea6bbcb38e993b7ac16824a84067d405c5
Abdullah Rafi <abdllahrfi@gmail.com>
2022-08-02 18:54:42 -0400

    change

--- src/hw5Q3Constructor/StudentTest.java
+++ src/hw5Q3Constructor/StudentTest.java
@@ -8,10 +8,5 @@ Student student=new Student();
 Student student2=new Student("Abdullah Ar Rafi",17,3.9875f,'M',false); 
 //17 is not taking as byte .  WHY??
 		
-	}
-	
-	
-	
-	
-	
+	}// for future 
 }
